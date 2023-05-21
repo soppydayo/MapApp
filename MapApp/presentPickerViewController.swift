c2105008664077a1e5a8286634b407359fad3881
SotaShirakawa <souta060614@gmail.com>
2023-05-22 00:08:39 +0900

    タブバーカメラボタンでカメラ起動

--- MapApp/presentPickerViewController.swift
+++ MapApp/presentPickerViewController.swift
@@ -7,23 +7,35 @@
 
 import UIKit
 
-class presentPickerViewController: UIViewController {
-
+class presentPickerViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        presentPickerController(sourceType: .camera)
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func presentPickerController(sourceType: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+            
+            
+            /*
+             // MARK: - Navigation
+             
+             // In a storyboard-based application, you will often want to do a little preparation before navigation
+             override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+             }
+             */
+            
+        }
     }
-    */
-
+    
 }
